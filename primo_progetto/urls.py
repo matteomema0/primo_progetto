ce08dcad0029f0adb1259dce50a45493065682f2
matteomema0 <matteo.mema@alessandrinimainardi.edu.it>
2024-11-27 11:07:13 +0100

    Aggiornamento passo 10

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from primo_progetto.views import index_root
 
+app_name = "primo_progetto"
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("prima_app.urls",namespace="prima_app"))
+    path('prima_app/',include("prima_app.urls",namespace="prima_app")),
+    path('',index_root),
+
 ]
