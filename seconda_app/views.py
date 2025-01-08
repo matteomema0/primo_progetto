98940ba966376b684b710089cf51e397a1c99177
matteomema0 <matteo.mema@alessandrinimainardi.edu.it>
2025-01-08 11:57:38 +0100

    punto 11

--- seconda_app/views.py
+++ seconda_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import datetime
 def es_if(request):
      context = { 
           'var1': 200, 
@@ -7,6 +7,20 @@ def es_if(request):
           'var3': 300 
           } 
      return render(request, "es_if.html", context)
+
+def if_else_elif(request):
+     context = { 
+          'var1': 100, 
+          'var2': 100.0, 
+          'var3': 100.50 
+          } 
+     return render(request, "if_else_elif.html", context)
+
+def es_for(request):
+     context = {'list1': [1, datetime.date(2019,7,16), 'Do not give up!'],
+                'list2': [1, datetime.date(2019,7,16), 'Do not give up!'] 
+     }
+     return render(request, "es_for.html", context)
 
 def index2(request):
     return render(request,"index2.html")
